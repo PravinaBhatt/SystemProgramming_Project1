353acab9fc62b73ec54b6a33f9b4c5417cad4ac2
PravinaBhatt <pravina.bhatt@hotmail.com>
2018-04-12 01:04:59 -0400

    Edited Server

--- SP_server.c
+++ SP_server.c
@@ -9,48 +9,49 @@
 #include <errno.h>
 #include <arpa/inet.h> 
 
-void child(int sd);
+void child(int s_des);
 int main(int argc, char *argv[]){
-  int sd, client, portNumber;
+  int s_des, remote_c, p_num;
   socklen_t len;
-  struct sockaddr_in servAdd;
+  struct sockaddr_in s_add,c_add;
 
   if(argc != 2){
    printf("Call model: %s <Port #>\n", argv[0]);
    exit(0);
   }
-  if ((sd=socket(AF_INET, SOCK_STREAM, 0))<0){
-   fprintf(stderr, "Cannot create sicket\n");
+  if ((s_des=socket(AF_INET, SOCK_STREAM, 0))<0){
+   fprintf(stderr, "Cannot create socket\n");
    exit(1);
   }
-  servAdd.sin_family = AF_INET;
-  servAdd.sin_addr.s_addr = htonl(INADDR_ANY);
-  sscanf(argv[1], "%d", &portNumber);
-  servAdd.sin_port = htons((uint16_t)portNumber);
-  bind(sd,(struct sockaddr*)&servAdd,sizeof(servAdd));
-  listen(sd, 5);
+  s_add.sin_family = AF_INET;
+  s_add.sin_addr.s_addr = htonl(INADDR_ANY);
+  sscanf(argv[1], "%d", &p_Num);
+  s_add.sin_port = htons((uint16_t)p_Num);
+  bind(s_des,(struct sockaddr*)&s_add,sizeof(s_add));
+  listen(s_des, 5);
 
   while(1){
-   client=accept(sd,(struct sockaddr*)NULL,NULL);
-   printf("Got a client\n");
+   remote_c=accept(s_des,(struct sockaddr*)&client_add,sizeof(c_add);
+   printf("***Client is ready for communication***\n");
    if(!fork())
-   child(client);
-   close(client);
+   child(remote_c);
+   close(remote_c);
   }
 }
 
-void child(int sd){
+void child(int s_des){
   char message[255];
+  struct c_add;
 
   while(1){
-   fprintf(stderr, "Enter line to send to client\n");
+   fprintf(stderr, "Enter message for a client:\n");
    fgets(message, 254, stdin);
-   write(sd, message, strlen(message)+1);
-   if(!read(sd, message, 255)){
-    close(sd);
-    fprintf(stderr,"Bye, client dead, wait for a new client\n");
+   write(s_des, message, strlen(message)+1);
+   if(!read(s_des, message, 255)){
+    close(s_des);
+    fprintf(stderr,"Ooops!! Current client is busy now\nWaiting for a new client...\n");
+    
     exit(0);
    }
-   fprintf(stderr,"Client send back: %s\n", message);
   }
 }
